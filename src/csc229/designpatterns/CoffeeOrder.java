2bf737516c4056a232341598245e1052bdb79fe5
Nathan Suver <nate.suver@gmail.com>
2023-04-15 11:32:19 -0400

    initial problem

---
+++ src/csc229/designpatterns/CoffeeOrder.java
@@ -0,0 +1,41 @@
+package csc229.designpatterns;
+
+public class CoffeeOrder {
+    private String size;
+    private String drink;
+    private double price;
+
+    public void OrderMyCoffee(String size, String drink) {
+        this.size = size;
+        this.drink = drink;
+        if (size.equals("small")) {
+            if (drink.equals("coffee")) {
+                this.price = 1.50;
+            } else if (drink.equals("latte") || drink.equals("americano")) {
+                this.price = 3.00;
+            }
+        } else if (size.equals("medium")) {
+            if (drink.equals("coffee")) {
+                this.price = 2.00;
+            } else if (drink.equals("latte") || drink.equals("americano")) {
+                this.price = 3.50;
+            }
+        } else if (size.equals("large")) {
+            if (drink.equals("coffee")) {
+                this.price = 2.50;
+            } else if (drink.equals("latte") || drink.equals("americano")) {
+                this.price = 4.00;
+            }
+        }
+    }
+
+    public void PrintOrder() {
+        System.out.println("Size: " + this.size + ", Drink: " + this.drink  + ", Price: $" + this.price);
+    }
+
+    public static void main(String[] args) {
+        CoffeeOrder coffee = new CoffeeOrder();
+		coffee.OrderMyCoffee("small","latte");
+        coffee.PrintOrder();
+	}
+}
---
+++ src/csc229/designpatterns/coffeeshop.java
@@ -0,0 +1,7 @@
+package csc229.designpatterns;
+
+public class coffeeshop {
+    public static void main(String[] args) {
+		
+	}
+}
