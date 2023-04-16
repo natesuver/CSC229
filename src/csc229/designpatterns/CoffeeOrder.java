6666482b79865f2bc33ec94f70d717d60b83317c
Nathan Suver <nate.suver@gmail.com>
2023-04-16 12:26:32 -0400

    add espresso

--- src/csc229/designpatterns/CoffeeOrder.java
+++ src/csc229/designpatterns/CoffeeOrder.java
@@ -13,18 +13,24 @@ public class CoffeeOrder {
                 this.price = 1.50;
             } else if (drink.equals("latte") || drink.equals("americano")) {
                 this.price = 3.00;
+            }  else if (drink.equals("espresso"))  {
+                this.price = 2.00;
             }
         } else if (size.equals("medium")) {
             if (drink.equals("coffee")) {
                 this.price = 2.00;
             } else if (drink.equals("latte") || drink.equals("americano")) {
                 this.price = 3.50;
+            }  else if (drink.equals("espresso"))  {
+                this.price = 2.50;
             }
         } else if (size.equals("large")) {
             if (drink.equals("coffee")) {
                 this.price = 2.50;
             } else if (drink.equals("latte") || drink.equals("americano")) {
                 this.price = 4.00;
+            }  else if (drink.equals("espresso"))  {
+                this.price = 3.50;
             }
         }
     }
@@ -35,7 +41,7 @@ public class CoffeeOrder {
 
     public static void main(String[] args) {
         CoffeeOrder coffee = new CoffeeOrder();
-		coffee.OrderMyCoffee("small","latte");
+		coffee.OrderMyCoffee("medium","espresso");
         coffee.PrintOrder();
 	}
 }
