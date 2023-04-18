3f05a03bc1cfdb170d737cb39fb2cabb17ec520a
Nathan Suver <nate.suver@gmail.com>
2023-04-17 20:16:06 -0400

    add a simple ui

--- src/csc229/designpatterns/decorator/CoffeeOrderWithDecorator.java
+++ src/csc229/designpatterns/decorator/CoffeeOrderWithDecorator.java
@@ -1,5 +1,7 @@
 package csc229.designpatterns.decorator;
 
+import java.util.Scanner;
+
 public class CoffeeOrderWithDecorator {
     private Beverage myDrink;
 
@@ -11,20 +13,78 @@ public class CoffeeOrderWithDecorator {
         myDrink.PrintOrder();
     }
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        Beverage beverage = new Espresso();
-		beverage.PrintOrder();
- 
-		Beverage beverage2 = new DarkRoast();
-		beverage2 = new Mocha(beverage2);
-		beverage2 = new Mocha(beverage2);
-		beverage2 = new Whip(beverage2);
-		beverage2.PrintOrder();
- 
-		Beverage beverage3 = new HouseBlend();
-		beverage3 = new Soy(beverage3);
-		beverage3 = new Mocha(beverage3);
-		beverage3 = new Whip(beverage3);
-		beverage3.PrintOrder();
+        Scanner sc= new Scanner(System.in);
+        String input;
+        while (true) {
+            System.out.println("Welcome to CoffeeBux!  Can I take your order?");
+            input = sc.nextLine();
+            if (input.equals("no")) {
+                System.out.println("Thank you!  Have a CoffeeTastic day!  Next Customer?");
+            }
+            else {
+                while (true) {
+                    System.out.println("What would you like?");
+                    input = sc.nextLine();
+                    boolean beverageFound = true;
+                    switch (input) {
+                        case "dark roast":
+                            System.out.println("You picked dark roast!");
+                            break;
+                        case "espresso":
+                            System.out.println("You picked espresso!");
+                            break;
+                        case "house blend":
+                            System.out.println("You picked house blend!");
+                            break;
+                        case "decaf":
+                            System.out.println("You picked decaf!");
+                            break;
+                        default:
+                            System.out.println("We do not have " + input + ".  Pick something else");
+                            beverageFound = false;
+                            break;
+                    }
+                    while (true && beverageFound) {
+                        System.out.println("What condiments you do want?");
+                        input = sc.nextLine();
+                        if (input.contains("none")) {
+                            System.out.println("Beverage Added to order!");
+                            break;
+                        }
+                        switch (input) {
+                            case "milk":
+                                System.out.println("You picked milk!");
+                                break;
+                            case "mocha":
+                                System.out.println("You picked mocha!");
+                                break;
+                            case "soy":
+                                System.out.println("You picked soy!");
+                                break;
+                            case "whip":
+                                System.out.println("You picked whip!");
+                                break;
+                            default:
+                                System.out.println("We do not have " + input + ". Pick something else." );
+                                break;
+                        }
+              
+                    }
+                    
+                    System.out.println("Would you like another beverage?");
+                    input = sc.nextLine();
+                    if (input.equals("no")) {
+                        System.out.println("Sending Order to Barista");
+                        System.out.println("Your total is $56.54.  Pickup your order at the counter.  Thank you and come again!  Next Customer?");
+                        break;
+                    }
+                }
+               
+            }
+        }
 	}
 }
